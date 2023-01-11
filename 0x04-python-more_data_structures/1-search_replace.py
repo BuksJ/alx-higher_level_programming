43d205865154885401e71550cf300147d643250f
bukasj <chukwuebukaedej@gmail.com>
2023-01-11 12:55:39 -0800

    replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    return [find if find != search else replace for find in my_list]
