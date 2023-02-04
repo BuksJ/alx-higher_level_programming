6a930ef8ad4c52eeda638c16f75c562b619d7f78
bukasj <chukwuebukaedej@gmail.com>
2023-02-04 08:01:01 -0800

    adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,12 +6,11 @@ It adds 2 integers a and b must be first casted"""
 def add_integer(a, b=98):
     """a and b are integers
     Returns an integer: the addition of a and b"""
-    if isinstance(a, float):
-        a = int(a)
-        if isinstance(b, float):
-            b = int(b)
-            if not (isinstance(a, int)):
-                raise TypeError("a must be an integer")
-            if not (isinstance(b, int)):
-                raise TypeError("b must be an integer")
-            return (a + b)
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or a float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or a float")
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
