5843489938ecf0a86cfa023859df05aad906748d
bukasj <chukwuebukaedej@gmail.com>
2023-03-14 17:30:47 -0700

    Introduction to SQL

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,5 @@
 #!/bin/bash
 --This script creates the database hbtn_0c_0 in your MySQL server
+--connect to MySQL server and execute query
 mysql -u root -p -e "/* Create database if not exists */
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;"
