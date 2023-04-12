e5ab24e2f92641265ce8ef17171d89127b9faf14
bukasj <chukwuebukaedej@gmail.com>
2023-04-11 19:46:44 -0700

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-'use strict';
-
-const argument = process.argv[2];
-
-if (isNaN(argument)) {
-console.log('Missing size');
-} else {
-const size = parseInt(argument);
-for (let i = 0; i < size; i++) {
-console.log('X'.repeat(size));
-}
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing size');
+} else if (parseInt(process.argv[2]) > 0) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
 }
