2f9bad586b30d68ec81a85d42f29496e98abce9d
bukasj <chukwuebukaedej@gmail.com>
2023-04-27 22:00:00 -0700

    script that sends a request to a URL passed as an argument, and displays only the status code of the response

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script sends a request to a URL passed as an argument,
+# and displays only the status code of the response
+# You are not allowed to use any pipe, redirection, etc
+# You are not allowed to use ; and &&
+curl -L -s -X HEAD -w "%{http_code}" "$1"
