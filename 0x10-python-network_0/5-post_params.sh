8678da1121c2cfa45c014efebc3708adf5ae863f
bukasj <chukwuebukaedej@gmail.com>
2023-04-27 21:23:33 -0700

    script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -3,4 +3,5 @@
 # and displays the body of the response
 # A variable email is sent with the value test@gmail.com
 # A variable subject is sent with the value I will always be here for PLD
-curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
+curl -sX POST "$1" -d "email=test@gmail.com&
+subject=I will always be here for PLD" -L
