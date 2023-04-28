5e64af27cb242eb3149464696ff07f2a741b85db
bukasj <chukwuebukaedej@gmail.com>
2023-04-27 21:50:16 -0700

    script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,4 +2,4 @@
 # this script takes in a URL, sends a GET request to the URL
 # and displays the body of the response
 # Displays only body of a 200 status code response
-curl -sX GET $1 -L
+curl -sX GET "$1" -L
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This sends a DELETE request to the URL passed as the first argument
 # and displays the body of the response
-curl -sX DELETE $1 -L
+curl -sX DELETE "$1" -L
