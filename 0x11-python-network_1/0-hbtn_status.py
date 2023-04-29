f150c274dc6c52d76c13614d350ee4f9b532367f
bukasj <chukwuebukaedej@gmail.com>
2023-04-28 17:56:55 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,13 @@ This module fetches https://alx-intranet.hbtn.io/status using urllib package
 and displays the body of the response.
 """
 
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(url) as response:
         html = response.read()
+        print("Body response:")
         print("- type: {}".format(type(html)))
         print("- content: {}".format(html))
         print("- utf8 content: {}".format(html.decode('utf-8')))
